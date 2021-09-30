f80ea45e68dd5f75c573ffd23339378751e39c3b
Hugo LC <hugolindoso.c@gmail.com>
2021-09-30 19:54:55 -0300

    Realizados alguns testes com as rotas

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -4,3 +4,25 @@ from app import app
 @app.route("/")
 def index():
     return "hello world"
+
+
+@app.route("/test", defaults={'name': None}) #define o valor default do name para none
+@app.route("/test/<name>") #passa um valor por padrão string pela url
+def test(name):
+    if name:
+        return "Olá, %s!" % name
+    else:
+        return "Olá, Usuário!"
+
+
+@app.route("/id/<int:id>")  # passa um valor inteiro pela url
+def id(id):
+    return "Seu Id é: %s!" % id
+
+@app.route("/parouimpar/<int:number>", methods=['GET']) #limita o metodo http a ser usado nessa rota
+def parouimpar(number):
+    restoDiv = number % 2
+    if restoDiv == 1:
+        return "Impar"
+    else:
+        return "Par"
